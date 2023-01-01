1b0ecc1076ebb1382e6e97236b00ad809bbc3ca8
masaki nakamura <gmk4n0n@gmail.com>
2023-01-02 05:55:03 +0900

    post request using provisional request in init()

--- JsonSwiftUI/ContentView.swift
+++ JsonSwiftUI/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var login = PostViewModel()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
--- JsonSwiftUI/ViewModel/PostViewModel.swift
+++ JsonSwiftUI/ViewModel/PostViewModel.swift
@@ -10,7 +10,41 @@ import Foundation
 class PostViewModel: ObservableObject {
     @Published var authenticated = 0
     
+    init() {
+        login(email: "eve.holt@reqres.in", password: "cityslicka")
+    }
+    
     func login(email: String, password: String) {
+        guard let url = URL(string: "https://reqres.in/api/login") else { return }
+        let parametros = ["email": email, "password": password]
+        let body = try! JSONSerialization.data(withJSONObject: parametros)
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = body
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let response = response {
+                print (response)
+            }
+            
+            guard let data = data else { return }
+            
+            do {
+                let datos = try JSONDecoder().decode(PostModel.self, from: data)
+                if !datos.token.isEmpty {
+                    DispatchQueue.main.async {
+                        self.authenticated = 1
+                    }
+                }
+                
+            } catch let error as NSError {
+                print ("error al hacer post", error.localizedDescription)
+                DispatchQueue.main.async {
+                    self.authenticated = 2
+                }
+            }
+            
+        }.resume()
     }
 }
